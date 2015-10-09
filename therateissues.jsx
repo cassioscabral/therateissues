601070c8a58931f50fb0ef7b6c42b677f66c6c82
Cassio Cabral <cassioscabral@gmail.com>
2015-10-09 18:06:55 -0300

    change js to jsx and add initial startup functions for the app

---
+++ therateissues.jsx
@@ -0,0 +1,20 @@
+if (Meteor.isClient) {
+  // This code is executed on the client only
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}
+
+if (Meteor.isServer) {
+
+}
+
+Meteor.methods({
+
+});
